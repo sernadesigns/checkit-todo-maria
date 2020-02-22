53805f4a5cda9d77fcc8621d0fa75c387ee93d35
sernadesigns <sernadesigns@gmail.com>
2020-02-21 23:53:25 -0800

    Adds TodosList view and template

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,7 +1,18 @@
 (function() {
-	var todoModel = new checkit.TodoModel();
-	todoModel.setContent('climb a mountain');
+	var todosModel = new checkit.TodosModel();
 
-	var view = new checkit.TodoView(todoModel);
+	var todoModel = new checkit.TodoModel();
+	todoModel.setContent('dinner date with Maria');
+	todosModel.add(todoModel);
+
+	var todoModel = new checkit.TodoModel();
+	todoModel.setContent('buy flowers');
+	todosModel.add(todoModel);
+
+	var todoModel = new checkit.TodoModel();
+	todoModel.setContent('take a shower');
+	todosModel.add(todoModel);
+
+	var view = new checkit.TodosListView(todosModel);
 	document.body.appendChild(view.build());
 }());
