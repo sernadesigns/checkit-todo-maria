f25bb3ab8f1a6ce30e7486afa753f0b2b8ebe191
sernadesigns <sernadesigns@gmail.com>
2020-02-23 00:53:53 -0800

    Adds delete done toolbar

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,9 +1,12 @@
-(function() {
+maria.on(window, 'load', function() {
 	var model = new checkit.TodosModel();
 
 	var view = new checkit.TodosInputView(model);
 	document.body.appendChild(view.build());
 
+	var view = new checkit.TodosToolbarView(model);
+	document.body.appendChild(view.build());
+
 	var view = new checkit.TodosListView(model);
 	document.body.appendChild(view.build());
-}());
+});
